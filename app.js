1e4afa6ed98badba3ef8e60bfa88432f7b2c2233
jomorro <morrowjoe0@gmail.com>
2019-02-07 12:58:46 -0500

    update to poll eevery 5 seconds and stop if successive chances are over 2 attempts.

--- app.js
+++ app.js
@@ -1,104 +1,89 @@
-const express = require("express");
-const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const port = 3000;
-const uploadsPath = "./public/uploads";
-const publicPath = "./public/";
+const uploadsPath = './public/uploads';
+const publicPath = './public/';
 
 const app = express();
 app.use(express.json());
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 app.use(express.static(publicPath));
 const storage = multer.diskStorage({
-  destination: uploadsPath,
-  filename: function(req, file, callback) {
-    const fileExtName = path.extname(file.originalname);
-    const newFilename =
-      file.originalname.replace(new RegExp(fileExtName), "") +
-      "-" +
-      Date.now() +
-      fileExtName;
-    callback(null, newFilename);
-  }
+    destination: uploadsPath,
+    filename: function (req, file, callback) {
+        const fileExtName = path.extname(file.originalname);
+        const newFilename = file.originalname.replace(new RegExp(fileExtName), '')
+            + '-' 
+            + Date.now() 
+            + fileExtName;
+        callback(null, newFilename);
+    }
 });
 
 const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter: function(req, file, callback) {
-    checkFileType(file, callback);
-  }
+    storage: storage,
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter: function (req, file, callback) {
+        checkFileType(file, callback);
+    }
 });
-
+//certain type of file updloads
 function checkFileType(file, callback) {
-  const filetypes = /jpg|jpeg|png|gif/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-  if (extname && mimetype) {
-    return callback(null, true);
-  } else {
-    callback("Error: Images only!");
-  }
+    const filetypes = /jpg|jpeg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (extname && mimetype) {
+        return callback(null, true);
+    } else {
+        callback('Error: Images only!');
+    }
 }
 
-app.get("/", (req, res) => {
-  fs.readdir(uploadsPath, function(err, items) {
-    if (err) {
-      return console.log(`Error reading directory ${uploadsPath} `, err);
-    }
-    const itemPaths = items
-      .map(item => `uploads/${item}`)
-      .map(imagePath => [fs.statSync("public/" + imagePath).mtimeMs, imagePath])
-      .sort(
-        (mTimeAndPath1, mTimeAndPath2) => mTimeAndPath2[0] - mTimeAndPath1[0]
-      )
-      .map(([imageMtime, imagePath]) => imagePath);
-    res.render("index", {
-      title: "KenzieGram",
-      h1: "Welcome to Kenziegram",
-      images: itemPaths
+app.get('/', (req, res) => {
+    fs.readdir(uploadsPath, function(err, items) {
+        if (err) {
+            return console.log(`Error reading directory ${uploadsPath} `, err);
+        }
+        const itemPaths = items
+            .map(item => `uploads/${item}`)
+            .map(imagePath => [fs.statSync('public/' + imagePath).mtimeMs, imagePath])
+            .sort((mTimeAndPath1, mTimeAndPath2) => mTimeAndPath2[0] - mTimeAndPath1[0])
+            .map(([imageMtime, imagePath]) => imagePath);
+        res.render('index', {title: 'KenzieGram', h1: 'Welcome to Kenziegram', images: itemPaths});
     });
-  });
 });
 
-app.post("/latest", (req, res) => {
-  let newestTimestamp = req.body.after;
-  fs.readdir(uploadsPath, function(err, items) {
-    if (err) {
-      return console.log(`Error reading directory ${uploadsPath} `, err);
-    }
-    const newImageMtimesAndPaths = items
-      .map(item => `uploads/${item}`)
-      .map(imagePath => [fs.statSync("public/" + imagePath).mtimeMs, imagePath])
-      .sort(
-        (mTimeAndPath1, mTimeAndPath2) => mTimeAndPath2[0] - mTimeAndPath1[0]
-      )
-      .filter(mTimeAndPath => mTimeAndPath[0] > newestTimestamp);
-    if (newImageMtimesAndPaths.length > 0) {
-      newestTimestamp = newImageMtimesAndPaths[0][0];
-      const newImagePaths = newImageMtimesAndPaths.map(
-        ([imageMtime, imagePath]) => imagePath
-      );
-      res.send({
-        images: newImagePaths,
-        timestamp: newestTimestamp
-      });
-    } else {
-      res.send({});
-    }
-  });
+app.post('/latest', (req, res) => {
+    let newestTimestamp = req.body.after;
+    fs.readdir(uploadsPath, function(err, items) {
+        if (err) {
+            return console.log(`Error reading directory ${uploadsPath} `, err);
+        }
+        const newImageMtimesAndPaths = items
+            .map(item => `uploads/${item}`)
+            .map(imagePath => [fs.statSync('public/' + imagePath).mtimeMs, imagePath])
+            .sort((mTimeAndPath1, mTimeAndPath2) => mTimeAndPath2[0] - mTimeAndPath1[0])
+            .filter(mTimeAndPath => mTimeAndPath[0] > newestTimestamp);
+        if (newImageMtimesAndPaths.length > 0) {
+            newestTimestamp = newImageMtimesAndPaths[0][0];
+            const newImagePaths = newImageMtimesAndPaths.map(([imageMtime, imagePath]) => imagePath);
+            res.send({
+                images: newImagePaths,
+                timestamp: newestTimestamp
+            });
+        } else {
+            res.send({});
+        }
+    }); 
 });
 
-app.post("/uploads/", upload.single("myImage"), (req, res) => {
-  res.render("upload", {
-    title: "Upload",
-    h1: "file uploaded",
-    imagePath: `uploads/${req.file.filename}`
-  });
+app.post('/uploads/', upload.single('myImage'), (req, res) => {
+    res.render('upload', {title: 'Upload', h1: 'file uploaded', imagePath: `uploads/${req.file.filename}`});
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}.`));
--- public/index.js
+++ public/index.js
@@ -1,21 +1,16 @@
-let lasttimestamp = Date.now();
-const url =window.location.href;
+lastTimestamp = Date.now();
+const url = window.location.href;
 let successiveFailures = 0;
 
+//pull every 5 seconds
 const interval = setInterval(checkForNewImagesAndUpdate, 5000);
 
 function checkForNewImagesAndUpdate() {
-    const newImagesArray = checkForNewImages(lasttimestamp);
+    const newImagesArray = checkForNewImages(lastTimestamp);
 }
 
 function checkForNewImages(timestamp) {
-    successiveFailures++;
-    if (successiveFailures > 2) {
-        clearInterval(interval);
-        document.getElementById('connection-lost').appendChild(document.createTextNode('Server Connection Lost'));
-        return;
-    }
-    fetch('${url}latest' , {
+    fetch(`${url}latest`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json"
@@ -29,10 +24,18 @@ function checkForNewImages(timestamp) {
             const arrayOfImagePaths = hydratedBody.images.reverse();
             for (let imagePath of arrayOfImagePaths) {
                 const imageNode = document.createElement('img');
-                imageNode.width = '450';
-                imageNode.src = '${imagePath}';
+                imageNode.width = '500';
+                imageNode.src = `${imagePath}`;
                 document.getElementById('images-container').insertBefore(imageNode, document.getElementById('images-container').firstChild);
             }
         }
+        //.catch if the fetch fails add to successfaliures
+    }).catch(err => {
+        successiveFailures++;
+        if (successiveFailures > 2) {
+            clearInterval(interval);
+            document.getElementById('connection-lost').appendChild(document.createTextNode('Server Connection Lost'));
+            return;
+        }   
     });
 }
